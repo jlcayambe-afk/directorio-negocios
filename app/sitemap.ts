3c543e1b2ef93d8ee9441427cffdee828e29578f
Jose Luis Cayambe <jlcayambe@gmail.com>
2026-08-14 15:31:15 -0500

    Agregar archivo sitemap dinamico

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://directorio-negocios-ecru.vercel.app";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/login`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/register`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
